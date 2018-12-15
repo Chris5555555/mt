fbc3421da60f829fd873d226d5312efe254dadd8
Chris5555555 <Chris_55555@163.com>
2018-12-15 10:00:53 +0800

    List组件创建

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Home from "./components/home"
 import My from "./components/my"
+import List from "./components/list"
 import { HashRouter as Router,Route,Link ,Switch,Redirect} from 'react-router-dom'
 class App extends Component {
   render() {
@@ -10,12 +11,14 @@ class App extends Component {
         <div className="App">
           <Switch>
             <Route path="/home"  component={Home} exact/>
-            <Route path="/my"  component={My} exact/>
+            <Route path="/my"  component={My} />
+            <Route path="/list"  component={List} />
             <Redirect from='/' to='/home'/>
           </Switch>
         
            <Link to="/home">首页</Link>
            <Link to="/my">我的</Link>
+           <Link to="/list">列表</Link>
         </div>
        </Router>
     
